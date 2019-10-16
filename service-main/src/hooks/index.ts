a98fd1fcbeb40ec6942eda2dfad385fba6478296
Brendan McNamara <me@brendan9.com>
2019-10-16 10:47:50 -0700

    Removes console

--- service-main/src/hooks/index.ts
+++ service-main/src/hooks/index.ts
@@ -4,7 +4,6 @@ import { Interface as InterfaceType } from '@brendan9/service-foundation';
 
 export const Hooks = [
   Interface.endpoints.root.createHook(async () => {
-    console.log('Calling hook');
     return InterfaceType.RESTResponse.Success({ hello: 'world' });
   }),
 ];
