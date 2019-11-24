21e257d6db528f6edb3419797b7ef77d4f265585
Brendan McNamara <me@brendan9.com>
2019-11-24 10:39:19 -0800

    Moves redux state from FileEditor -> ContentViewer

--- web-fs/src/ContentViewer.tsx
+++ web-fs/src/ContentViewer.tsx
@@ -3,18 +3,91 @@ import './ContentViewer.css';
 import FileEditor from './file-editor';
 import React, { useState } from 'react';
 
+import { addDocument } from './store/actions';
+import { DocumentContent, Model as Document } from './store/Document.model';
+import { EditorState } from 'draft-js';
+import { State as State$EditMode } from './store/editMode.reducer';
+import { useDispatch, useSelector } from './store';
+import { useThrottle } from './throttle';
+
 const ContentViewer: React.FC = () => {
   const [title, setTitle] = useState('Untitled Document');
 
-  function onChangeTitle(title: string) {
-    setTitle(title);
-  }
+  const reduxState = useSelection();
+  const dispatch = useDispatch();
+
+  const onChangeTitle = useThrottle(1000, (title: string) => {
+    const { editMode } = reduxState;
+    switch (editMode.type) {
+      case 'NEW_DOCUMENT': {
+        const document = Document.createLocal({
+          groups: [],
+          name: title,
+        });
+        dispatch(addDocument(document, ''));
+        break;
+      }
+    }
+  });
+
+  const onChangeEditorState = useThrottle(1000, (editorState: EditorState) => {
+    const { editMode } = reduxState;
+    switch (editMode.type) {
+      case 'NEW_DOCUMENT': {
+        const document = Document.createLocal({
+          groups: [],
+          name: title,
+        });
+
+        dispatch(
+          addDocument(document, editorState.getCurrentContent().getPlainText()),
+        );
+        break;
+      }
+
+      case 'EDIT_DOCUMENT': {
+        break;
+      }
+    }
+  });
 
   return (
     <div className="ContentViewer-root">
-      <FileEditor onChangeTitle={onChangeTitle} title={title} />
+      <FileEditor
+        onChangeEditorState={onChangeEditorState}
+        onChangeTitle={onChangeTitle}
+        title={title}
+      />
     </div>
   );
 };
 
 export default ContentViewer;
+
+// -----------------------------------------------------------------------------
+// State Selection
+// -----------------------------------------------------------------------------
+
+interface StateSelection {
+  document: Document | undefined;
+  documentContent: DocumentContent;
+  editMode: State$EditMode;
+}
+
+function useSelection(): StateSelection {
+  return useSelector(state => {
+    const { editMode } = state;
+
+    const document =
+      editMode.type === 'EDIT_DOCUMENT'
+        ? state.documents.documents[editMode.documentRef.refID]
+        : undefined;
+
+    const documentContent =
+      editMode.type === 'EDIT_DOCUMENT'
+        ? state.documents.documentContents[editMode.documentRef.refID] || ''
+        : '';
+
+    return { document, documentContent, editMode };
+  });
+}
--- web-fs/src/file-editor/FileEditor.tsx
+++ web-fs/src/file-editor/FileEditor.tsx
@@ -5,75 +5,27 @@ import FileEditorContent from './FileEditorContent';
 import FileEditorTitle from './FileEditorTitle';
 import React from 'react';
 
-import { addDocument } from '../store/actions';
-import { DocumentContent, Model as Document } from '../store/Document.model';
 import { EditorState } from 'draft-js';
-import { useDispatch, useSelector } from '../store';
-import { State as State$EditMode } from '../store/editMode.reducer';
-import { useThrottle } from '../throttle';
 
 interface Props {
+  onChangeEditorState: (editorState: EditorState) => void;
   onChangeTitle: (title: string) => void;
   title: string;
 }
 
 const FileEditor: React.FC<Props> = (props: Props) => {
-  const reduxState = useSelection();
-  const dispatch = useDispatch();
-
-  const onChangeTitle = useThrottle(1000, (title: string) => {
-    console.log(reduxState.editMode.type);
-    switch (reduxState.editMode.type) {
-      case 'NEW_DOCUMENT': {
-        const document = Document.createLocal({
-          groups: [],
-          name: title,
-        });
-        dispatch(addDocument(document, ''));
-        break;
-      }
-    }
-  });
-
   function onEnterTitle() {}
-
-  const onChangeEditorState = useThrottle(
-    1000,
-    (editorState: EditorState) => {},
-  );
 
   return (
     <div className={classnames('FileEditor-root')}>
       <FileEditorTitle
-        onChange={onChangeTitle}
+        onChange={props.onChangeTitle}
         onEnter={onEnterTitle}
-        title="Untitled Document"
+        title={props.title}
       />
-      <FileEditorContent onChange={onChangeEditorState} />
+      <FileEditorContent onChange={props.onChangeEditorState} />
     </div>
   );
 };
 
 export default FileEditor;
-
-// -----------------------------------------------------------------------------
-// State Selection
-// -----------------------------------------------------------------------------
-
-interface StateSelection {
-  documentContent: DocumentContent;
-  editMode: State$EditMode;
-}
-
-function useSelection(): StateSelection {
-  return useSelector(state => {
-    const { editMode } = state;
-
-    const documentContent =
-      editMode.type === 'EDIT_DOCUMENT'
-        ? state.documents.documentContents[editMode.documentRef.refID] || ''
-        : '';
-
-    return { documentContent, editMode };
-  });
-}
